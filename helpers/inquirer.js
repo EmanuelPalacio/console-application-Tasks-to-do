0f66883aa47c978ab498518112eedbd2a45dc1b2
Emanuel Palacio <emanuel.palacio.arq@gmail.com>
2023-01-25 16:05:42 -0300

    interactive menu options

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -4,21 +4,61 @@ export const inquirerMenu = async () => {
   const question = [
     {
       type: "list",
-      name: "opción",
+      name: "option",
       message: "¿Que desea hacer?",
-      choices: ["A", "B", "C"],
+      choices: [
+        {
+          value: 1,
+          name: "1. Crear tarea",
+        },
+        {
+          value: 2,
+          name: "2. Listar tareas",
+        },
+        {
+          value: 3,
+          name: "3. Listar tareas completadas",
+        },
+        {
+          value: 4,
+          name: "4. Lista tareas pendientes",
+        },
+        {
+          value: 5,
+          name: "5. Completar tarea(s)",
+        },
+        {
+          value: 6,
+          name: "6. Borrar tarea",
+        },
+        {
+          value: 0,
+          name: "0. Salir",
+        },
+      ],
     },
   ];
 
   console.clear();
   console.log(
-    "==========================================================".cyan
+    "==========================================================".yellow
   );
-  console.log("   Selecione una opción".cyan);
+  console.log("   Selecione una opción".yellow);
   console.log(
-    "==========================================================\n".cyan
+    "==========================================================\n".yellow
   );
-  const opt = await inquirer.prompt(question);
+  const { option } = await inquirer.prompt(question);
 
-  return opt;
+  return option;
+};
+export const pause = async () => {
+  const question = [
+    {
+      type: "input",
+      name: "enter",
+      message: `Presiona ${"ENTER".yellow} para continuar`,
+    },
+  ];
+  console.log("\n");
+  await inquirer.prompt(question);
 };
